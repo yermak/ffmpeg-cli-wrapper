c70ff5a0da49b9b2085bb2466ab2cc01f62d508e
yermak <yermak@gmail.com>
2020-03-31 11:51:18 +0100

    integrated changes from https://github.com/MaxMoto1702/ffmpeg-cli-wrapper added chapter support

---
+++ src/main/java/net/bramp/ffmpeg/probe/FFMpegChapterTag.java
@@ -0,0 +1,12 @@
+
+package net.bramp.ffmpeg.probe;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class FFMpegChapterTag {
+
+  @SerializedName("title")
+  @Expose
+  public String title;
+}
